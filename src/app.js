a86d1582bc785612872a9b940ec13a89c197ea3b
Pablo Spector <pablo_spector@hotmail.com>
2022-12-17 13:08:13 -0300

    second commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import path from 'path';
+// import path from 'path';
 import morgan from 'morgan'
 import cors from 'cors';
 
 const app = express();
-app.use(cors())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(morgan('dev'));
 
 // app.use(express.static(path.join(__dirname, '/public')));
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import app from './app';
-import SocketIO from 'socket.io';
+import app from './app.js';
+import { Server as SocketIO } from 'socket.io';
 
 const port = process.env.PORT || 4000
 
@@ -7,7 +7,7 @@ const server = app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
 });
 
-const io = SocketIO(server, {
+const io = new SocketIO(server, {
   cors: {
     origin: '*'
   }
@@ -15,12 +15,12 @@ const io = SocketIO(server, {
 
 io.on('connection', (socket) => {
   console.log(`connection ID: ${socket.id}`);
-  
+
   socket.on('message', (data) => {
     io.sockets.emit('message', data)
   })
 
-  socket.on('typing', (data) =>{
+  socket.on('typing', (data) => {
     socket.broadcast.emit('typing', data);
   })
 })
